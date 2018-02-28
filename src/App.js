b216e7e704acba7c1831e69f5ba724c74ecce94d
Brian K. Jones <bkjones@gmail.com>
2018-02-27 22:58:39 -0500

    refactor: only indentation updates
    App.js indentation was kind of a shit show.

--- src/App.js
+++ src/App.js
@@ -8,37 +8,38 @@ import { Link } from 'react-router-dom'
 class BooksApp extends React.Component {
   render() {
     return (
-      <div className="app">
+        <div className="app">
             <Route path='/search' render={({history}) => (
-                  <div className="search-books">
+                <div className="search-books">
                     <div className="search-books-bar">
                         <Link
                             to='/'
                             className="close-search"
                         >Close</Link>
-                      <div className="search-books-input-wrapper">
-                        <input type="text" placeholder="Search by title or author"/>
-                      </div>
+                        <div className="search-books-input-wrapper">
+                            <input type="text" placeholder="Search by title or author"/>
+                        </div>
                     </div>
                     <div className="search-books-results">
-                      <ol className="books-grid"></ol>
+                        <ol className="books-grid"></ol>
                     </div>
-                  </div>
+                </div>
             )}/>
-                <Route exact path='/' render={({ history }) => (
-                    <div>
-                        <div className="list-books-title">
-                          <h1>MyReads</h1>
-                        </div>
-                        <div className='list-library-contents'>
-                            <Library />
-                            <div className="open-search">
-                                <Link to='/search'>Add a book</Link>
-                            </div>
+            
+            <Route exact path='/' render={({ history }) => (
+                <div>
+                    <div className="list-books-title">
+                        <h1>MyReads</h1>
+                    </div>
+                    <div className='list-library-contents'>
+                        <Library />
+                        <div className="open-search">
+                            <Link to='/search'>Add a book</Link>
                         </div>
                     </div>
-                )}/>
-      </div>
+                </div>
+            )}/>
+        </div>
         )}
   }
 
