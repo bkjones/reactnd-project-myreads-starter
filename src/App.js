54461343db28102e9a5622ce39bdf00a74df73b6
Brian K. Jones <bkjones@gmail.com>
2018-03-01 00:38:51 -0500

    feat: Search logs results from API
    Nothing shows on the page yet, but I'm updating the query & getting
    results from the API, which I'm able to see in the log.
    
    I can also see that I should've stuck with my initial implementation
    that had a flat list of books in the Library state instead of the more
    complicated 'bookShelves' object :/

--- src/App.js
+++ src/App.js
@@ -2,9 +2,32 @@ import React from 'react'
 import './App.css'
 import Library from './Library'
 import { Route, Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 
 class BooksApp extends React.Component {
+    state = {
+        query: ''
+    }
+
+    updateQuery = (query) => {
+        this.setState({ query: query })
+    }
+
+    clearQuery = () => {
+        this.setState({query: ''})
+    }
+
     render() {
+        let searchResults = []
+        let query = this.state.query
+        if (query){
+            BooksAPI.search(query).then((response) => {
+                console.log("Search response: ", response)
+                searchResults = response
+            })
+        } else {
+            searchResults = []
+        }
         return (
             <div className="app">
                 <Route path='/search' render={({history}) => (
@@ -14,12 +37,21 @@ class BooksApp extends React.Component {
                                 to='/'
                                 className="close-search"
                             >Close</Link>
-                            <div className="search-books-input-wrapper">
-                                <input type="text" placeholder="Search by title or author"/>
+                            <div className="search-books-input">
+                                <input
+                                    type='text'
+                                    placeholder='Search by title or author'
+                                    value={query}
+                                    onChange={(event) => this.updateQuery(event.target.value)}
+                                />
                             </div>
                         </div>
                         <div className="search-books-results">
-                            <ol className="books-grid"></ol>
+                            <ol className="books-grid">
+                                {searchResults.map(book =>
+                                    <li>{book}</li>
+                                )}
+                            </ol>
                         </div>
                     </div>
                 )}/>
