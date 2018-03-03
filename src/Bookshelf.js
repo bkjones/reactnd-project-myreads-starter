51c06dfea34c0bd206ca6c15f07305d3cdf52d7a
Brian K. Jones <bkjones@gmail.com>
2018-03-03 13:05:31 -0500

    fix: Shelves were switching positions when books were moved
    I wanted a more general approach to assembling the supported bookshelves
    that would allow for easy expansion by the user later (a user could
    easily add whatever shelves they wanted). That's not a requirement,
    though, and assembling them on each render caused whole shelves to
    switch position in the UI randomly, which is weird. I took that
    functionality out and replaced it with a fixed mapping of short to
    prettified shelf names in Bookshelf.js.
    
    This also allowed for a bit of code cleanup in both Library and
    Bookshelf.

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react'
 import Book from './Book'
 
 class Bookshelf extends Component {
+    state = {
+        shelfNames: {
+            currentlyReading: "Currently Reading",
+            wantToRead: "Want to Read",
+            read: "Read"
+        }
+    }
+
     render() {
         return (
             <div className="bookshelf">
-              <h2 className="bookshelf-title">{this.props.shortName}</h2>
+              <h2 className="bookshelf-title">{this.state.shelfNames[this.props.shortName]}</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
                   {this.props.books.filter(b => b.shelf === this.props.shortName).map(book =>
--- src/Library.js
+++ src/Library.js
@@ -4,20 +4,15 @@ import * as BooksAPI from './BooksAPI'
 
 class Library extends Component {
     /*
-    * The Library component will be composed of Bookshelf components.
-    * It produces the main page view, and should be able to handle state
-    * changes like moving a book from one bookshelf to another.
-    *
-    * When we create the search page, we can use a Library component on that
-    * page as well, with only one bookshelf present to show search results.
-    * That way, you can perform the same actions on a book from either screen.
+    The Library component holds Bookshelf components, which hold Book components
     */
     state = {
         books: [],
+        shelfShortNames: ['currentlyReading', 'wantToRead', 'read']
     }
 
     changeBookshelf = (bookId, e) => {
-        const newShelf = e.target.value
+        const newShelf = e.target.value //caching to use asynchronously
         let updatedBooks = []
 
         BooksAPI.update({id: bookId}, newShelf).then(() => {
@@ -36,25 +31,15 @@ class Library extends Component {
 
     componentDidMount(){
         /* Get all books & put them in the proper shelves. */
-        //BooksAPI.getAll().then(books => console.log("BOOKS: ", books))
         BooksAPI.getAll().then(books => this.setState({books}))
     }
 
-    getUniqueShelfNames = (books) => {
-        console.log("books in getUniqueShelfNames: ", books)
-        let shelfNames = new Set()
-        books.map(book => shelfNames.add(book.shelf))
-        console.log("shelfNames: ", shelfNames)
-        return shelfNames
-    }
-
     render() {
-        const shelfNames = this.getUniqueShelfNames(this.state.books)
         return(
           <div className="list-books-content">
             <div>
                 {
-                    Array.from(shelfNames).map(shelf =>
+                    this.state.shelfShortNames.map(shelf =>
                         <div key={shelf}>
                         <Bookshelf
                             shortName={shelf}
